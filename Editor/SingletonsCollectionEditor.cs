7d1601a4423c50329ddecab05561f4a3d4a9f978
Janek <jasiojfd@gmail.com>
2024-08-25 10:53:13 +0200

    added a scriptable object singleton

--- Editor/SingletonsCollectionEditor.cs
+++ Editor/SingletonsCollectionEditor.cs
@@ -1,30 +1,18 @@
 ﻿using SingletonSystem.Runtime;
 using UnityEditor;
-using UnityEngine;
 
-namespace SingletonSystem.Editor{
-    public static class SingletonsCollectionEditor{
+namespace SingletonSystem.Editor
+{
+    public static class SingletonsCollectionEditor
+    {
 #if UNITY_EDITOR
 
+        /// <summary>
+        /// Open the SingletonsCollection in the inspector
+        /// </summary>
         [MenuItem("Tools/Singleton System/Open SingletonsCollection")]
         private static void OpenBindingsCollection() => EditorUtility.OpenPropertyEditor(SingletonsCollection.instance);
-
-        internal static SingletonsCollection CreateBindingsCollection(){
-            var singletonsCollection = ScriptableObject.CreateInstance<SingletonsCollection>();
-            singletonsCollection.name = "SingletonsCollection";
-
-            if (!AssetDatabase.IsValidFolder("Assets/Resources"))
-                AssetDatabase.CreateFolder("Assets", "Resources");
-
-            if (!AssetDatabase.IsValidFolder("Assets/Resources/SingletonSystem"))
-                AssetDatabase.CreateFolder("Assets/Resources", "SingletonSystem");
-
-            AssetDatabase.CreateAsset(singletonsCollection, "Assets/Resources/SingletonSystem/SingletonsCollection.asset");
-            AssetDatabase.SaveAssets();
-
-            return singletonsCollection;
-        }
-
+        
 #endif
     }
 }
---
+++ Runtime/ScriptableObjectSingleton.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace SingletonSystem.Runtime
+{
+    /// <summary>
+    /// Base class for scriptable object singletons.
+    /// </summary>
+    public abstract class ScriptableObjectSingleton : ScriptableObject { }
+
+    /// <summary>
+    /// Adds singleton functionality to a scriptable object.
+    /// </summary>
+    /// <typeparam name="T">The type of the scriptable object singleton.</typeparam>
+    public abstract class ScriptableObjectSingleton<T> : ScriptableObjectSingleton where T : ScriptableObjectSingleton<T>
+    {
+        /// <summary>
+        /// The instance of the scriptable object singleton.
+        /// </summary>
+        public static T instance
+        {
+            get
+            {
+                if (_instance) return _instance;
+
+                if (SingletonsCollection._instance)
+                    foreach (var soSingleton in SingletonsCollection._instance.scriptableObjectSingletons.Where(
+                                 soSingleton => soSingleton.GetType() == typeof(T)))
+                        return _instance = soSingleton as T;
+
+                _instance = Resources.Load<T>(typeof(T).Name);
+
+                if (_instance) return _instance;
+
+#if UNITY_EDITOR
+                _instance = CreateScriptableObject();
+                
+                static T CreateScriptableObject()
+                {
+                    var soInstance = CreateInstance<T>();
+                    soInstance.name = typeof(T).Name;
+
+                    if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/Resources"))
+                        UnityEditor.AssetDatabase.CreateFolder("Assets", "Resources");
+
+                    // if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/Resources/SingletonSystem"))
+                    //     UnityEditor.AssetDatabase.CreateFolder("Assets/Resources", "SingletonSystem");
+
+                    UnityEditor.AssetDatabase.CreateAsset(soInstance, $"Assets/Resources/{typeof(T).Name}.asset");
+                    
+                    UnityEditor.AssetDatabase.SaveAssets();
+
+                    return soInstance;
+                }
+#endif
+
+                return _instance;
+            }
+        }
+
+        private static T _instance;
+    }
+}
